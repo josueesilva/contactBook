2e2e16a5bcd7e2368a7e25c1f456fb1d58b1daff
Josue <josueelias94@gmail.com>
2017-02-16 00:30:27 -0200

    insert contact

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,3 +7,15 @@ Template.body.helpers({
     return  Contacts.find({});
   }
 });
+Template.body.events(
+  {
+    'click .enviamanolo'(event){
+      debugger;
+      event.preventDefault();
+      const name =document.querySelectorAll('.new-contact')[0].name.value;
+      const numero = document.querySelectorAll('.new-contact')[0].number.value;
+      const tp = document.querySelectorAll('.new-contact')[0].tp.value;
+
+    },
+  }
+);
