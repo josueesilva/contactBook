b3a1bc42793ecbcdd41f4dd1e57fe0e610786832
Josue <josueelias94@gmail.com>
2017-02-18 08:31:52 -0200

    restarting after  undo commit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,13 +9,23 @@ Template.body.helpers({
 });
 Template.body.events(
   {
-    'click .enviamanolo'(event){
+    'click .insertContact'(event, instance){
       debugger;
       event.preventDefault();
-      const name =document.querySelectorAll('.new-contact')[0].name.value;
-      const numero = document.querySelectorAll('.new-contact')[0].number.value;
-      const tp = document.querySelectorAll('.new-contact')[0].tp.value;
+      // const name =document.querySelectorAll('.new-contact')[0].name.value;
+      // const number = document.querySelectorAll('.new-contact')[0].number.value;
+      // const tp = document.querySelectorAll('.new-contact')[0].tp.value;
 
-    },
+      const name    = instance.find('#name').value;
+      const number  = instance.find('#number').value;
+      const tp      = instance.find('#tp').value
+
+      Contacts.insert({
+        name,
+        number,
+        tp,
+        createdAt: new Date(),
+       });
+     },
   }
 );
